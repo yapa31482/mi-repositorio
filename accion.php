0b7645328328b3b67d77467090986e8fc06d7cbe
yapa31482 <yapamipid@hotmail.com>
2023-01-18 09:34:26 -0500

    mi primer archivo

---
+++ accion.php
@@ -0,0 +1,7 @@
+<?php 
+
+echo $_POST["nombre"] ;
+echo $_POST["edad"];
+
+
+?>
---
+++ index.php
@@ -0,0 +1,25 @@
+
+<hml>
+    <head>
+
+
+    </head>
+<body>
+
+<form action="accion.php" method="post">
+
+<label>Nombre</label><input type="text" name="nombre">
+<label>Edad</label><input type="text" name="edad">
+<input type="submit"value="Enviar">
+
+</form>
+
+</body>
+
+</hml>
+
+<?php
+
+
+
+?>
